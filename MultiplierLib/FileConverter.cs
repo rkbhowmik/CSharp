9b90e712552a252e4a632683ca3bc0c610109444
rkbhowmik <rajib.bhowmik@gmail.com>
2017-09-11 14:21:04 +0200

    string comparison and then creating a two text files named before and after to compare.

---
+++ MultiplierLib/FileConverter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiplierLib
+{
+	public class FileConverter
+	{
+		public string Transform(string row)
+		{
+			string[] tokens = row.Split(';');
+			var firstName = tokens[0].ToUpper();
+			var lastName = tokens[1].ToUpper();
+			var years = DateTime.Now.Year - int.Parse(tokens[2]);
+			var message = $"{firstName} {lastName} ÄR {years} ÅR GAMMAL.";
+			return message;
+		}
+
+		public void TransformFile(string inputName, string outputName) //TransformFile is a new method
+		{
+			StringWriter sw = new StringWriter();
+			string[] lines = FileConverter.ReadAllLines(inputName);
+			foreach (var line in lines)
+			{
+				sw.WriteLine(Transform(line));
+			}
+			sw.Close();
+			FileConverter.WriteAllText(outputName, sw.ToString());
+		}
+	}
+
+}
---
+++ MultiplierTestLib/FileConversionTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MultiplierLib;
+
+namespace MultiplierTestLib
+{
+	[TestClass]
+	public class FileConversionTests
+	{
+		[TestMethod]
+		public void mattias_asplund_1971()
+		{
+			var sut = new FileConverter();
+			string actual = sut.Transform("mattias;asplund;1971");
+			Assert.AreEqual("MATTIAS ASPLUND ÄR 46 ÅR GAMMAL.", actual);
+		}
+		[TestMethod]
+		public void amanda_asplund_2003()
+		{
+			var sut = new FileConverter();
+			string actual = sut.Transform("amanda;asplund;2003");
+			Assert.AreEqual("AMANDA ASPLUND ÄR 14 ÅR GAMMAL.", actual);
+		}
+	}
+}
