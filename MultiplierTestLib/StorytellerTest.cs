7278019b8d6fde5d4e429cf5f775da02d05b42dd
rkbhowmik <rajib.bhowmik@gmail.com>
2017-09-13 12:49:50 +0200

    Created Storryteller class and StorytellerTest method

---
+++ MultiplierTestLib/StorytellerTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MultiplierLib;
+
+namespace MultiplierTestLib
+{
+	[TestClass]
+	public class StorytellerTest
+	{
+		[TestMethod]
+		public void TellStoryYoung()
+		{
+			//Arrange
+			var sut = new Storyteller(); //sut= System under test
+			var nameOf
+			//Act
+
+			//Assert
+		}
+	}
+}
