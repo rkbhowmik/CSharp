9719c9370eff9fec19e3d2898c47287a33575969
rkbhowmik <rajib.bhowmik@gmail.com>
2017-09-20 09:11:24 +0200

    storyteller test

--- MultiplierLib/Storyteller.cs
+++ MultiplierLib/Storyteller.cs
@@ -8,6 +8,23 @@ namespace MultiplierLib
 {
 	public class Storyteller
 	{
+		public string TellStory(string nameOfChild, int ageOfChild, bool willBeScary)
+		{
+			if (ageOfChild < 5 && willBeScary)
+				throw new InvalidOperationException();
+			string prefix = "";
+			if (ageOfChild < 7)
+				prefix += $"Lyssna nu {nameOfChild}. Det var en gång en prinsessa ";
+			else
+				prefix += $"Hallå {nameOfChild}! En gång fanns en kille ";
 
+			string suffix = "";
+			if (!willBeScary)
+				suffix += "som tyckte om att dansa.";
+			else
+				suffix += "som egentligen var en varulv.";
+
+			return prefix + suffix;
+		}
 	}
 }
--- MultiplierTestLib/StorytellerTest.cs
+++ MultiplierTestLib/StorytellerTest.cs
@@ -8,14 +8,33 @@ namespace MultiplierTestLib
 	public class StorytellerTest
 	{
 		[TestMethod]
-		public void TellStoryYoung()
+		public void TellStoryToYoungNotScary()
 		{
-			//Arrange
-			var sut = new Storyteller(); //sut= System under test
-			var nameOf
-			//Act
-
-			//Assert
+			var sut = new Storyteller();
+			string actual = sut.TellStory("Alexander", 6, false);
+			Assert.AreEqual($"Lyssna nu Alexander. Det var en gång en prinsessa som tyckte om att dansa.", actual);
 		}
+		[TestMethod]
+		public void TellStoryToOldNotScary()
+		{
+			var sut = new Storyteller();
+			string actual = sut.TellStory("Linda", 8, false);
+			Assert.AreEqual($"Hallå Linda! En gång fanns en kille som tyckte om att dansa.", actual);
+		}
+		[TestMethod]
+		public void TellStoryToOldScary()
+		{
+			var sut = new Storyteller();
+			string actual = sut.TellStory("Linda", 8, true);
+			Assert.AreEqual($"Hallå Linda! En gång fanns en kille som egentligen var en varulv.", actual);
+		}
+		[TestMethod]
+		[ExpectedException(typeof(InvalidOperationException))]
+		public void TellStoryToYoungScary()
+		{
+			var sut = new Storyteller();
+			string actual = sut.TellStory("Peter", 4, true);
+		}
+
 	}
 }
