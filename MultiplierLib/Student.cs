f88166232b238bfb00234d52f7fb8f23916c0b55
rkbhowmik <rajib.bhowmik@gmail.com>
2017-09-20 09:35:54 +0200

    -ExamTests- Statistics method has been created to--UnassignedStudent method created -

---
+++ MultiplierLib/Student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiplierLib
+{
+	public class Student
+	{
+		private string v;
+
+		public Student(string v)
+		{
+			this.v = v;
+		}
+	}
+}
---
+++ MultiplierTestLib/ExamTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MultiplierLib;
+using System.Collections.Generic;
+
+namespace MultiplierTestLib
+{
+	[TestClass]
+	public class ExamTests
+	{
+		[TestMethod]
+		public void Statistics()
+		{
+			var sut = new Exam("CSharp");
+			var alice = new Student("Alice");
+			var bob = new Student("Bob");
+			var caesar = new Student("Caesar");
+			sut.Assign(alice);
+			sut.Assign(bob);
+			sut.Assign(caesar);
+			sut.Grade(alice, "G");
+			sut.Grade(bob, "VG");
+			sut.Grade(caesar, "G");
+			Dictionary<string, int> statistics = sut.GenerateStatistics();
+			Assert.AreEqual(statistics["IG"], 0);
+			Assert.AreEqual(statistics["G"], 2);
+			Assert.AreEqual(statistics["VG"], 1);
+		}
+		[TestMethod]
+		[ExpectedException(typeof(UnassignedStudentException))]
+		public void UnassignedStudent()
+		{
+			var sut = new Exam("CSharp");
+			Student alice = new Student("Alice");
+			Student bob = new Student("Bob");
+			sut.Grade(alice, "G");
+		}
+	}
+}
