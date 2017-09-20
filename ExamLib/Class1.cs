a67cbdc1340cc522f388df2b031f2efd9c3f286c
rkbhowmik <rajib.bhowmik@gmail.com>
2017-09-20 09:42:22 +0200

    New project has been created with method name and class name

---
+++ ExamLib/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace ExamLib
+{
+    public class Class1
+    {
+    }
+}
---
+++ ExamTestsLib/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ExamTestsLib")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ExamTestsLib")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("5b3c3593-51a0-4105-93bf-c055c83e9705")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
