4e939f59e4cfb04abf08f987e9b2e852b590f091
rkbhowmik <rajib.bhowmik@gmail.com>
2017-09-25 09:08:08 +0200

    GuessNumber

---
+++ GuessNumberLib/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace GuessNumberLib
+{
+    public class Class1
+    {
+    }
+}
---
+++ GuestNumberTestLib/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("GuestNumberTestLib")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("GuestNumberTestLib")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ebd5b75c-5d05-4869-9f79-4debaea5fbab")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
