0e0ba993490b6da4bfdaff74b87ba8b78b182a56
rkbhowmik <rajib.bhowmik@gmail.com>
2017-10-02 13:23:23 +0200

    -slideshow app done

--- SlideShowApp/Form1.Designer.cs
+++ SlideShowApp/Form1.Designer.cs
@@ -28,20 +28,51 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
+			this.components = new System.ComponentModel.Container();
+			this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.timer1 = new System.Windows.Forms.Timer(this.components);
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
 			this.SuspendLayout();
+			// 
+			// openFileDialog1
+			// 
+			this.openFileDialog1.FileName = "openFileDialog1";
+			this.openFileDialog1.Multiselect = true;
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(405, 206);
+			this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.pictureBox1.TabIndex = 0;
+			this.pictureBox1.TabStop = false;
+			// 
+			// timer1
+			// 
+			this.timer1.Interval = 3000;
+			this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
 			// 
 			// Form1
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(500, 261);
+			this.Controls.Add(this.pictureBox1);
 			this.Name = "Form1";
 			this.Text = "Form1";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
 			this.ResumeLayout(false);
 
 		}
 
 		#endregion
+
+		private System.Windows.Forms.OpenFileDialog openFileDialog1;
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.Timer timer1;
 	}
 }
 
--- SlideShowApp/Form1.cs
+++ SlideShowApp/Form1.cs
@@ -12,9 +12,33 @@ namespace SlideShowApp
 {
 	public partial class Form1 : Form
 	{
+		private int fileNumber;
 		public Form1()
 		{
 			InitializeComponent();
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			openFileDialog1.ShowDialog();
+			timer1.Enabled = true;
+			
+		}
+
+		private void timer1_Tick(object sender, EventArgs e)
+		{
+			DisplayFileNumber(fileNumber);
+			fileNumber++;
+			if (fileNumber>= openFileDialog1.FileNames.Count())
+			{
+				fileNumber = 0;
+			}
+		}
+
+		private void DisplayFileNumber(int fileNumber)
+		{
+			pictureBox1.ImageLocation = openFileDialog1.FileNames[fileNumber];
+			pictureBox1.Load();
 		}
 	}
 }
