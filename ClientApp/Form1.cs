f8a5537bb3e1a5be3dc7cbc2bc5934c524b3d535
rkbhowmik <rajib.bhowmik@gmail.com>
2017-10-04 10:17:39 +0200

    -Take name and social security number and check whether its male/female and show greetings with name

---
+++ ClientApp/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ClientApp
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void label1_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void label2_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			string pNumber = textBox3.Text.Substring(9, 1);
+			int numVal = Int32.Parse(pNumber);
+			if (numVal % 2 == 1)
+			{
+				label4.Text = "God Morgon herr" + " "+ textBox1.Text + textBox2.Text;
+			}
+			else
+			{
+				label4.Text = "God Morgon fröken" + " " + textBox1.Text + textBox2.Text;
+			}
+		}
+
+		private void textBox3_TextChanged(object sender, EventArgs e)
+		{
+
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+
+		}
+	}
+}
---
+++ KundUppgifterApp/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KundUppgifterApp
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void textBox1_TextChanged(object sender, EventArgs e)
+		{
+			
+		}
+
+		private void label1_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void label2_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			saveFileDialog1.ShowDialog();
+			var name = textBox1.Text;
+			var phone = textBox2.Text;
+			var publish = name + ";" + dateTimePicker1.Text + ";" + phone;
+			File.WriteAllText(saveFileDialog1.FileName, publish);
+
+		}
+
+		private void textBox2_TextChanged(object sender, EventArgs e)
+		{
+
+		}
+
+		private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+		{
+
+		}
+		
+	}
+}
