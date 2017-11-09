a20a58dbaf1533ff795bad8346733e8cd528e353
rajib.bhowmik <rajib.bhowmik@gmail.com>
2017-11-09 20:01:17 +0100

    - Form name has been changed

--- ConvertToUpperCaseApp/Form1.Designer.cs
+++ ConvertToUpperCaseApp/Form1.Designer.cs
@@ -66,7 +66,7 @@
 			this.Controls.Add(this.button2);
 			this.Controls.Add(this.button1);
 			this.Name = "Form1";
-			this.Text = "Form1";
+			this.Text = "Conver one file\'s content to uppercase";
 			this.Load += new System.EventHandler(this.Form1_Load);
 			this.ResumeLayout(false);
 
